fb3b3c57d647d23cddc8b9452aed9b460f4f09b6
AlonBnZn <136967077+AlonBnZn@users.noreply.github.com>
2025-07-16 17:38:14 +0300

    Delete shared/dist directory

--- shared/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./types";
-export * from "./utils/constants";
-export type { IndexingLog, DashboardFilters, DashboardMetrics, ClientPerformance, TimeSeriesData, ChatMessage, ChatResponse, APIResponse, } from "./types";
-export { DEFAULT_PAGINATION, DATE_FORMATS, PROGRESS_METRICS, SUPPORTED_COUNTRIES, LOG_STATUSES, CHART_COLORS, } from "./utils/constants";
-//# sourceMappingURL=index.d.ts.map
